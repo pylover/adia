3b2d92d3b81c5039715eb9a76483d33449d49550
pylover <vahid.mardani@gmail.com>
2021-06-30 22:20:38 +0430

    The patch for the brython-dev/brython#1715 has been removed.

--- dial/container.py
+++ dial/container.py
@@ -1,6 +1,3 @@
 class Container(list):
     def __init__(self, *args, **kwargs):
-        # FIXME: Wont's pass the args to the list due the brython bug:
-        # https://github.com/brython-dev/brython/issues/1715
-        # super().__init__(*args, **kwargs)
-        super().__init__()
+        super().__init__(*args, **kwargs)
