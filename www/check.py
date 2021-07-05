73f427fbac845f391076711ed13ca265a2b19d14
pylover <vahid.mardani@gmail.com>
2021-07-05 22:01:43 +0430

    Some test files has been splitted.

--- www/check.py
+++ www/check.py
@@ -13,24 +13,30 @@ def runtests(module):
 def run():
     from tests import test_token
     from tests import test_tokenizer
-    from tests import test_asciicanvas
     from tests import test_mutablestring
     from tests import test_sequence
+    from tests import test_asciicanvas
+    from tests import test_asciirenderer
+    from tests import test_asciidiagram
     from tests import test_diagram
-    from tests import test_ascii_renderer
-    from tests import test_ascii_diagram
-    from tests import test_ascii_sequence
+    from tests import test_ascii_sequence_header
+    from tests import test_ascii_sequence_callstack
+    from tests import test_ascii_sequence_condition
+    from tests import test_ascii_sequence_loop
 
     for module in [
         test_token,
         test_tokenizer,
-        test_asciicanvas,
         test_mutablestring,
         test_sequence,
         test_diagram,
-        test_ascii_renderer,
-        test_ascii_diagram,
-        test_ascii_sequence,
+        test_asciicanvas,
+        test_asciirenderer,
+        test_asciidiagram,
+        test_ascii_sequence_header,
+        test_ascii_sequence_callstack,
+        test_ascii_sequence_condition,
+        test_ascii_sequence_loop,
     ]:
         for test in runtests(module):
             yield test
