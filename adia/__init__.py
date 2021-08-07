a5157132c7ed0921343dbd063f8c63fdd27c19a9
pylover <vahid.mardani@gmail.com>
2021-08-08 00:11:13 +0430

    v0.2.2

--- adia/__init__.py
+++ adia/__init__.py
@@ -11,7 +11,7 @@ from .exceptions import InterpreterError, BadAttribute, BadSyntax
 from .renderer import Renderer
 
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __all__ = [
     'Diagram',
     'SequenceDiagram',
