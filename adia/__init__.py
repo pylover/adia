8b3d4e4d8874b9c099c28cf4a8fd499bcd88f17e
pylover <vahid.mardani@gmail.com>
2021-08-09 00:48:14 +0430

    Playing with gh pages deploy

--- adia/__init__.py
+++ adia/__init__.py
@@ -11,7 +11,7 @@ from .exceptions import InterpreterError, BadAttribute, BadSyntax
 from .renderer import Renderer
 
 
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 __all__ = [
     'Diagram',
     'SequenceDiagram',
