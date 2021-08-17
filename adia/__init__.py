c86719452935f8e8efaf308c8a81fb104fcdff86
pylover <vahid.mardani@gmail.com>
2021-08-17 20:49:42 +0430

    Fix: JS API & it's documentation

--- adia/__init__.py
+++ adia/__init__.py
@@ -11,7 +11,7 @@ from .exceptions import InterpreterError, BadAttribute, BadSyntax
 from .renderer import Renderer
 
 
-__version__ = '2.0.2'
+__version__ = '2.0.3'
 __all__ = [
     'Diagram',
     'SequenceDiagram',
