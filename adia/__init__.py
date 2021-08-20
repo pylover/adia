42ce422e6b274c6aa0c21a1d29ffa4f67d1df3a1
pylover <vahid.mardani@gmail.com>
2021-08-20 13:02:12 +0430

    Backward incompatible changes on JS API

--- adia/__init__.py
+++ adia/__init__.py
@@ -11,7 +11,7 @@ from .exceptions import InterpreterError, BadAttribute, BadSyntax
 from .renderer import Renderer
 
 
-__version__ = '3.0.1'
+__version__ = '4.0.0'
 __all__ = [
     'Diagram',
     'SequenceDiagram',
